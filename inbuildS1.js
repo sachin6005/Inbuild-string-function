9c3c5894f03685d13016757f0d69748ec24cf382
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 19:29:07 +0530

    problems are done on inbuild string functions

---
+++ inbuildS1.js
@@ -0,0 +1,15 @@
+var mStr = "This is my test string to practice JavaScript string function concepts. This is gonna be fun"
+
+console.log('This is my test string to practice JavaScript string function concepts'.length)
+
+console.log('This is gonna be fun'.length)
+
+console.log(mStr.length)
+
+console.log(mStr.indexOf('This'))
+
+console.log(mStr.lastIndexOf('This'))
+
+console.log(mStr.substr(0,12))
+
+console.log(mStr.substr(-12))
---
+++ inbuildS2.js
@@ -0,0 +1,9 @@
+var str1 = 'Today is'
+var str2 = 'A beautiful day'
+var str3 = 'In Hawaii'
+
+console.log(str1.concat(' ',str2.toLowerCase(),' ',str3.toLowerCase()))
+
+
+var mstr = 'Mo Tu We Th Fr Sa Su'
+console.log(mstr.toUpperCase().split(" "))
